b737d6054af901d11fb37f60eccca1fb36b0dc42
weijihong <weijihong@xiaoyouzi.com>
2020-03-06 00:26:49 +0800

    支持生成阈值的点

--- online_monitor.js
+++ online_monitor.js
@@ -1,14 +1,14 @@
 // 基于准备好的dom，初始化echarts实例
 thresholdMap = { "ANR": "0.6", "Crash率": 0.06, "卡顿": 6 }
 currentTargetLine = 0
-whichTarget = ''
+currentLegend = ''
 var myChart = echarts.init(document.getElementById('main'), null, { renderer: 'svg' });
 var appName = "美柚";
 var date = ["2020-01-06", "2020-01-07", "2020-01-08", "2020-01-09", "2020-01-10", "2020-01-11", "2020-01-12"];
 var anr = ["0.58", "0.59", "0.64", "0.73", "0.77", "0.77", "0.78"];
 var crash = ["0.05", "0.06", "0.04", "0.07", "0.10", "0.09", "0.09"];
 var lagging = ["6.19", "5.83", "5.95", "6.58", "6.33", "6.76", "6.38"];
-
+dataMap = { "ANR": anr, "Crash率": crash, "卡顿": lagging }
 var option = {
     title: {
         text: appName
@@ -93,7 +93,20 @@ var option = {
                         formatter: '{c}%'
                     }
                 }
-            }
+            },
+            markPoint: {
+                symbol: 'circle',
+                symbolSize: 10,
+                label: {
+                    show: true,
+                    formatter: "!",
+                    fontSize: 12
+                },
+                itemStyle: {
+                    color: '#00CD68'
+                },
+                data: null
+            },
         },
         {
             name: 'Crash率',
@@ -107,7 +120,20 @@ var option = {
                         formatter: '{c}%'
                     }
                 }
-            }
+            },
+            markPoint: {
+                symbol: 'circle',
+                symbolSize: 10,
+                label: {
+                    show: true,
+                    formatter: "!",
+                    fontSize: 12
+                },
+                itemStyle: {
+                    color: '#00CD68'
+                },
+                data: null
+            },
         },
         {
             name: '卡顿',
@@ -124,6 +150,7 @@ var option = {
                 itemStyle: {
                     color: '#00CD68'
                 },
+                data: null
             },
             itemStyle: {
                 normal: {
@@ -139,8 +166,7 @@ var option = {
 // 使用刚指定的配置项和数据显示图表。
 myChart.setOption(option);
 myChart.on('legendselectchanged', function (params) {
-    //判断select里是否只有一个true
-    // selected: {ANR: true, Crash率: true, 卡顿: false}
+    console.log("监听legend改变")
     var selected = params.selected;
     let origin = []
     Object.keys(selected).forEach(function (item) {
@@ -148,8 +174,10 @@ myChart.on('legendselectchanged', function (params) {
             origin.push(item);
         }
     });
+    console.log("origin length is :" + origin.length);
     if (origin.length === 1) {
         //展示阈值参考线
+        currentLegend = origin[0]
         option.yAxis.axisPointer.handle.show = true;
         option.yAxis.axisPointer.value = thresholdMap[origin[0]]
         option.tooltip.triggerOn = 'none';
@@ -167,8 +195,36 @@ function mouseup() {
     //更改markpoint操作
     // 源和阈值
     console.log("阈值：" + currentTargetLine)
-    var selected = option.legend.selected;
-    console.log(selected)
+    console.log(currentLegend)
+    let datasource = dataMap[currentLegend]
+    console.log("datasource is " + datasource);
+    var markArr = [], temObj = null;
+    if (datasource != null) {
+        datasource.forEach(function (v, i) {
+            if (v > currentTargetLine) {
+                temObj = {
+                    value: v,
+                    xAxis: date[i],
+                    yAxis: v,
+                    itemStyle: {
+                        color: '#FF0B00'
+                    }
+                };
+            } else {
+                temObj = {};
+            }
+            markArr.push(temObj);
+
+
+        });
+        // 动态设置markpoint的形状
+        console.log(markArr)
+        option.series[1].markPoint.data = markArr
+        option.yAxis.axisPointer.value = currentTargetLine
+        myChart.setOption(option);
+    }
+
+
 
 
 }
